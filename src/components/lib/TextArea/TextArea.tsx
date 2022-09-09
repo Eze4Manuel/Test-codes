16bb2456149c53d198c882c91b84d9772c1fb413
Ayodeji Akinborewa <ayodejiakinborewa@gmail.com>
2022-09-09 10:58:38 +0100

    fix: redefined props

--- src/components/lib/TextArea/TextArea.tsx
+++ src/components/lib/TextArea/TextArea.tsx
@@ -3,28 +3,13 @@ import type { FC } from 'react';
 import styles from './TextArea.module.scss';
 import type TextAreaProps from './TextArea.props';
 
-const TextArea: FC<TextAreaProps> = ({
-  name,
-  label,
-  rows,
-  cols,
-  value,
-  onChange,
-  className,
-  required,
-  ...rest
-}) => {
+const TextArea: FC<TextAreaProps> = ({ name, label, className, ...rest }) => {
   return (
     <label htmlFor={name}>
       {label && <span className={styles.label}>{label}</span>}
       <textarea
         id={name}
         className={`${styles.textarea} ${className || ''}`}
-        rows={rows}
-        cols={cols}
-        value={value}
-        required={required}
-        onChange={onChange}
         {...rest}
       ></textarea>
     </label>
