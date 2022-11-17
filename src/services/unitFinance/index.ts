e8f2cb8d9ee26a2e39666e2374e7abf36cd4e32d
mightyfzeus <bolarinolabisi36@gmail.com>
2022-11-17 14:03:24 +0100

    fix: used correct campus id for the request

--- src/services/unitFinance/index.ts
+++ src/services/unitFinance/index.ts
@@ -2,7 +2,7 @@ import { getRequest } from '@/utils/api/calls';
 
 const getAllFinanceHistory = (query: string) => {
   return getRequest({
-    url: `/finance-history/search-history?campus=25eecb0c-2dd5-44ff-bfe1-4d11a58882f6&unit=FOLLOW_UP_UNIT&year=2023&month=${query}`,
+    url: `/finance-history/search-history?campus=3cece26c-e6c2-485a-9caa-9432be17b4be&unit=FOLLOW_UP_UNIT&year=2023&month=${query}`,
   });
 };
 
